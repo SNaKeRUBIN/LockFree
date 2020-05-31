359a2cb11c286ae8422c38e7d2f76836f1d0edd3
SNaKeRUBIN <pannu.abhinoor@gmail.com>
2020-05-30 22:31:38 -0400

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+// act as test for DS implementations
+// act as benchmarks for DS implementations
+
+// DS TO IMPLEMENT
+// queue as array
+// linked list
+// stack as array
+
+// LockFree DS TO IMPLEMENT
+//  queue as array
+//  linked list
+//  stack as array
+
+// Refs
+//  https://github.com/facebook/folly/blob/master/folly/ProducerConsumerQueue.h
+//  https://www.youtube.com/watch?v=ZQFzMfHIxng&t=3871s
+//  https://github.com/craflin/LockFreeQueue
+//  https://github.com/dodng/fast_ring_queue
+//  https://github.com/supermartian/lockfree-queue
+//  https://github.com/kevinlynx/lockfree-list
+
+int main()
+{
+    return 0;
+}
